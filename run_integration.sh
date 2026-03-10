90c2bb62ca12c56614175ab02b2e2123be28d192
Mateusz Maciaszek <mateusz.maciaszekhpc@gmail.com>
2026-03-10 23:29:16 +0100

    Remove unused run_integration.sh

--- run_integration.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Run integration tests with all mechanisms enabled
-set -euo pipefail
-OPEN_ECON=true \
-NBP_QE=true \
-NBP_FX_INTERVENTION=true \
-IO_MODE=enabled \
-IO_SCALE=0.5 \
-sbt "testOnly sfc.engine.IntegrationFullSpec"
